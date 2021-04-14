86c4635e88fe4498f1efad6b520e931b920eccbc
Samuel-Abusa <samuelabusakennedy@gmail.com>
2021-04-14 13:53:52 +0300

    Managed to create a function that prompts the user to key in their date of birth then converts it to a particular day of the week

--- scripts.js
+++ scripts.js
@@ -1,4 +1,14 @@
+let dateBorn;
+var dob;
+
 function collectDateBorn(){
-  alert("Hello there! Let's get started right away. Click ok to continue.")
+  alert("Hello there! Let's get started right away. Click ok to continue.");
+  
+  let dateBorn = prompt("Kindly enter the date you were born in the following formart : mm/dd/yyyy");
+
+  var dob = new Date(dateBorn);
+
+  return dob;
 }
 
+var dayOfTheWeek = collectDateBorn().getDay();
