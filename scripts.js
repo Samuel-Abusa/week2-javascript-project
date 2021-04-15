9207a73f3340f83c8b2feeb1dcacd88da867ec1b
Samuel-Abusa <samuelabusakennedy@gmail.com>
2021-04-15 08:29:29 +0300

    created a javascript function that takes the user in put and converts it into names

--- scripts.js
+++ scripts.js
@@ -1,7 +1,5 @@
-//Defined two variables that we'll use in the function
-
-let dateBorn;
-var dob;
+let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
 //This function runs on the event that the user clicks on the button with the event listener attribute
 
@@ -10,14 +8,36 @@ function collectDateBorn(){
   
   let dateBorn = prompt("Kindly enter the date you were born in the following formart : mm/dd/yyyy");
 
-  var dob = new Date(dateBorn);
+  //This varriable holds the data which the user keyed in and converted it from a string to date
+  let dtbrn = new Date(dateBorn);
+  
+   //After the data is collected from the user in form of a date we convert it into the index of the day of the week 0 being Sunday.
+  var dayOfTheWeek = dtbrn.getDay();
 
-  return dob;
+  var gender = prompt("What is your gender");
+
+  if (gender == "male"){
+    console.log(maleNames[dayOfTheWeek]);
+  }
+  else if (gender = "female"){
+    console.log(femaleNames[dayOfTheWeek]);
+  }
+  else{
+    alert("Error generating birth name")
+  }
 }
 
-//After the data is collected from the user in form of a date we convert it into the index of the day of the week 0 being Sunday.
-
-var dayOfTheWeek = collectDateBorn().getDay();
 
 
-var gender = prompt("What is your gender");
+
+
+
+  
+ 
+  
+
+  
+
+
+
+
