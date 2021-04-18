316133647e0c7aa6018c539c95389d8c3f2e3062
Samuel-Abusa <samuelabusakennedy@gmail.com>
2021-04-18 13:57:37 +0300

    added final touches to the js code

--- scripts.js
+++ scripts.js
@@ -1,49 +1,23 @@
-
-$ (document).ready(function(){
-  $ ('.card').css({"display" : "none"});
-});
-
-$ (document).ready(function(){
-  $ ('.dateBrn').click(function(){
-      $ ('.card').css({"display" : "block"});
-    }
-  );
-});
-
-let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-
-//This function runs on the event that the user clicks on the button with the event listener attribute
-
-/*function collectDateBorn(){
-  alert("Hello there! Let's get started right away. Click ok to continue.");
+var aknNames = function(yr, gendr){
+  let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+  var dt = new Date(yr);
   
-  let dateBorn = prompt("Kindly enter the date you were born in the following formart : mm/dd/yyyy");
-
-  //This varriable holds the data which the user keyed in and converted it from a string to date
-  let dtbrn = new Date(dateBorn);
-  
-  //After the data is collected from the user in form of a date we convert it into the index of the day of the week 0 being Sunday.
-  var dayOfTheWeek = dtbrn.getDay();
-  var monthOfTheYear = dtbrn.getMonth();
-  
-  if (dayOfTheWeek < 0 || dayOfTheWeek > 6 || monthOfTheYear < 0 || monthOfTheYear > 11){
-    alert("Invalid date format.");
+  if (gendr = "Male"){
+    return maleNames[dt.getDay()];
   }
   else{
-    var gender = prompt("What is your gender");
- 
-   if (gender == "male"){
-     console.log(maleNames[dayOfTheWeek]);
-   }
-   else if (gender = "female"){
-     console.log(femaleNames[dayOfTheWeek]);
-   }
-   else{
-     alert("Error generating birth name")
-   }
+    return femaleNames[dt.getDay()];
   }
 }
-*/
 
 
+
+$ (document).ready(function(){
+  $ ('form').submit(function(){
+    var yr = parseInt($ ('#do_b').val());
+    var gendr = $ ('#gndr').val();
+    
+    alert('Your Akan name is '+aknNames(yr, gendr));
+  });
+});
